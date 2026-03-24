f1ef30c7249344bf4dcc216f72132c8ece7f3403
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2026-03-24 17:31:05 +0800

    feat: add client portal and its features/tab

---
+++ src/app/(portal)/portal/client-gateway/activity-log/page.tsx
@@ -0,0 +1,6 @@
+// src/app/(portal)/portal/client-gateway/activity-log/page.tsx
+import { ClientGatewayActivityLog } from '@/components/client-gateway/ClientGatewayActivityLog';
+
+export default function ClientGatewayActivityLogPage() {
+  return <ClientGatewayActivityLog />;
+}
---
+++ src/app/(portal)/portal/client-gateway/announcements/page.tsx
@@ -0,0 +1,6 @@
+// src/app/(portal)/portal/client-gateway/announcements/page.tsx
+import { ClientGatewayAnnouncements } from '@/components/client-gateway/ClientGatewayAnnouncements';
+
+export default function ClientGatewayAnnouncementsPage() {
+  return <ClientGatewayAnnouncements />;
+}
