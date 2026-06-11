908eb93d226a60c5ad9a8bbd8902764a47d20885
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2026-06-11 08:15:20 +0800

    commented out the allowed IP

--- src/app/api/dashboard/timesheet/me/route.ts
+++ src/app/api/dashboard/timesheet/me/route.ts
@@ -143,10 +143,10 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
-  const ALLOWED_OFFICE_IPS = [
+  /*const ALLOWED_OFFICE_IPS = [
     "49.145.37.126", // PLDT Wifi
     "113.19.181.57", // Converge Wifi
-  ];
+  ];*/
 
  const forwarded = request.headers.get("x-forwarded-for");
   const realIp = request.headers.get("x-real-ip");
